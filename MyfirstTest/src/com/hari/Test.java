aef3285d87cdf09f57d92e8dc5338e65fc1369bb
harim <harim@LAPTOP-E7KEGIKJ>
2021-02-27 13:53:53 +0530

    commit test

--- MyfirstTest/src/com/hari/Test.java
+++ MyfirstTest/src/com/hari/Test.java
@@ -3,7 +3,7 @@ package com.hari;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("My first java programe");
+		System.out.println("My first java programe....");
 
 	}
 
